8114feb446819bc367cf5e7826d04bf30ba5222b
MSTL-Updates <98383663+MSTL-Updates@users.noreply.github.com>
2023-05-21 10:00:53 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 
 url_list = {}
-api_key = "45512504aa247c3c939d10f30b731c32"
+api_key = "d4987d75dfd24b8a36916bc28ed7a948812058b9"
 
 
 def search_movies(query):
@@ -32,7 +32,7 @@ def get_movie(query):
         links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
         final_links = {}
         for i in links:
-            url = f"https://shrtfly.com/api?api={api_key}&url={i['href']}"
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
             response = requests.get(url)
             link = response.json()
             final_links[f"{i.text}"] = link['shortenedUrl']
